21b66b2f2f9e26bfe7d6ab15e1ab9305c75b360b
samirulcodes <samirulislam.official.com>
2024-07-24 13:36:52 +0530

    picture path

---
+++ script.js
@@ -0,0 +1,48 @@
+// access key
+const accessKey="z4GxYAkvxQEEm0Q_cj5wRaYy7AyaxVi5a88Ygm0O6UY"
+
+const searchForm=document.getElementById('search-form')
+const searchBox=document.getElementById('search-box')
+const searchResult=document.getElementById('search-result')
+const showMoreBtn=document.getElementById('show-more-btn')
+
+
+let keyword=""
+let page=1
+
+// API
+async function searchImages(){
+    keyword=searchBox.value //it will store the value that we will enter in input field
+
+    // with this api url we are adding page number using${page} and keyword using ${keyword} and accesKey using ${accessKey}
+    const url=`https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}`
+
+    const response=await fetch(url)
+    const data=await response.json()
+
+    const results=data.results
+
+    results.map((result)=>{
+        const image=document.createElement("img")
+        image.src=result.urls.small // this img is fetch from api of unsplash and urls.small means only small imgages will show
+
+        const imageLink=document.createElement("a")
+        imageLink.href=result.links.html  //  it will add link in a tag and link is fetching from api
+
+        imageLink.target="_blank" // open in new tab
+
+        // placed img in a tag
+        imageLink.appendChild(image)
+
+        // a tag will be displayed in search-result id 
+        imageLink.appendChild(imageLink)
+    })
+
+}
+
+// search form
+searchForm.addEventListener("submit",(e)=>{
+    e.preventDefault()
+    page=1  // everytime search new keyword it will start form page 1
+    searchImages()
+})
